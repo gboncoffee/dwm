21fa8652d71f5a1ab4f67bc92feb5a96d4003899
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-06-10 12:19:16 -0300

    Revert "Added light colorscheme"
    This reverts commit 36cd8b3d81a071d81b53eeb73c34719cd9977772.

--- dark.h
+++
@@ -1,5 +0,0 @@
-/* Catppuccin Mocha (dark) */
-
-static const char col_fg[]  = "#CDD6F4";
-static const char col_bg[]  = "#1E1E2E";
-static const char col_sel[] = "#F5C2E7";
--- light.h
+++
@@ -1,5 +0,0 @@
-/* Catppuccin Latte (light) */
-
-static const char col_fg[]  = "#4C4F69";
-static const char col_bg[]  = "#EFF1F5";
-static const char col_sel[] = "#EA76CB";
