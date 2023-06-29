7fd0f0abbe2f00fd83b5db97e2bcec04e614a00a
Gabriel G. de Brito <gabrielgbrito@icloud.com>
2023-06-28 23:05:25 -0300

    Less border and less gap

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx = 3;        /* border pixel of windows */
-static const Gap default_gap        = {.isgap = 1, .realgap = 10, .gappx = 10};
+static const unsigned int borderpx = 2;        /* border pixel of windows */
+static const Gap default_gap       = {.isgap = 1, .realgap = 7, .gappx = 7};
 static const unsigned int snap     = 32;       /* snap pixel */
 static const int showbar           = 1;        /* 0 means no bar */
 static const int topbar            = 1;        /* 0 means bottom bar */
